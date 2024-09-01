a82e728e58ce82d28a7eef81a5abf2c4db80a441
Carlos Reyes <carlosalbertoreyeszuniga@gmail.com>
2024-09-01 11:56:00 -0600

    Plantillas agregadas

--- inicio.py
+++ inicio.py
@@ -1,15 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+publicaciones = [
+     {
+          'autor': 'Carlos Reyes',
+          'titulo': 'Publicacion 1',
+          'contenido': 'Primera publicación de la plataforma',
+          'fecha_publicada': '1 de agosto de 2024'
+     },
+     {
+          'autor': 'Marcos Guzman',
+          'titulo': 'Publicacion 2',
+          'contenido': 'Segunda publicación de la plataforma',
+          'fecha_publicada': '2 de agosto de 2024'
+     }
+]
 
 @app.route("/")
 @app.route("/inicio")
 def inicio():
-    return "<h1>Home Page</h1>"
+    return render_template ('inicio.html', titulo_pagina='Inicio')
+
+@app.route("/publicaciones")
+def blog_publicaciones():
+    return render_template ('publicaciones.html', publicaciones=publicaciones, titulo_pagina='Publicaciones')
+
+@app.route("/nutripedia")
+def nutripedia():
+    return render_template ('nutripedia.html', titulo_pagina='Nutripedia')
 
 @app.route("/acerca_de")
 def acerca_de():
-    return "<h1>Acerca de nosotros</h1>"
+    return render_template ('acerca_de.html', titulo_pagina='Acerca de')
 
 
 
