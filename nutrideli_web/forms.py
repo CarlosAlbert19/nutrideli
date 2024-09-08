c42ef26d13f906bbf217eb3ed299bb011ac23d80
Carlos Reyes <carlosalbertoreyeszuniga@gmail.com>
2024-09-08 13:10:14 -0600

    Actualización de la cuenta y foto de perfil

--- nutrideli_web/forms.py
+++ nutrideli_web/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import InputRequired, DataRequired, Length, Email, EqualTo, ValidationError
 from nutrideli_web.models import User
@@ -25,3 +27,22 @@ class Iniciar_sesion(FlaskForm):
     password = PasswordField('Contraseña', validators=[DataRequired()])
     remember = BooleanField('Recuérdame')
     submit = SubmitField('Iniciar sesión')
+
+
+class Actualizar_informacion(FlaskForm):
+    username = StringField('Nombre de usuario', validators=[DataRequired(), Length(min=2, max=25)])
+    email = StringField('Correo', validators=[DataRequired(), Email()])
+    picture = FileField('Actualizar foto de perfil', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Actualizar')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('Este nombre de usuario ya se encuentra en uso. Por favor, ingrese uno diferente.')
+        
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Ese correo ya se encuentra en uso. Por favor, ingrese uno diferente.')
