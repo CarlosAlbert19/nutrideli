21883a5c2d1affaf950aece6d3281806a6a978bd
Carlos Reyes <carlosalbertoreyeszuniga@gmail.com>
2024-09-22 13:09:57 -0600

    Páginas de error personalizadas

--- nutrideli_web/__init__.py
+++ nutrideli_web/__init__.py
@@ -26,8 +26,10 @@ def create_app(config_class=Config):
     from nutrideli_web.users.routes import users
     from nutrideli_web.posts.routes import posts
     from nutrideli_web.main.routes import main
+    from nutrideli_web.errors.handlers import errors
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
+    app.register_blueprint(errors)
 
     return app
